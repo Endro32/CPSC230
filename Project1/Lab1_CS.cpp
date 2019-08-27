b7641473bda0216b3262ecbef48cb79d1bd4f8c8
Stefan Carlson <webk727@gmail.com>
2019-08-27 13:39:22 -0400

    Added Lab 1

---
+++ Project1/Lab1_CS.cpp
@@ -0,0 +1,27 @@
+/*
+Stefan Carlson
+Soft Drink Profit Program
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int casesSoldDaily;
+
+	cout << "How many cases do you sell daily?\n";
+	cin >> casesSoldDaily;
+
+	double dailyProfit = casesSoldDaily * 2.4;
+	double annualProfit = dailyProfit * 365;
+
+	cout << "You will earn $";
+	cout << dailyProfit;
+	cout << " in one day.\n You will earn $";
+	cout << annualProfit;
+	cout << " in one year.";
+	
+	return 0;
+}
--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -7,16 +7,36 @@ Starting Up
 
 using std::cin;
 using std::cout;
+using std::string;
+
+char toLowerCase(char c);
 
 int main()
 {
 	//vars
+	string lastName;
+	double balance;
 
 	//input
+	cout << ""
+
 
 	//do stuff
+	string x = "XHello";
+
+	char y = x[0];
+	cout << toLowerCase(y);
 
 	//output
 
+	//cout << (int) 'A';
+	//cout << (int) 'Z';
+
 	return 0;
+}
+
+char toLowerCase(char c)
+{
+	if (c < 65 || c > 90) return -1;
+	return c + 32;
 }
