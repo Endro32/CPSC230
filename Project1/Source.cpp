cc064d424f7885337ef46e703e0f6daa8df06bb9
Endro32 <webk727@gmail.com>
2019-08-28 08:57:10 -0400

    Finished intial version of Source.cpp

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -14,10 +14,6 @@ char toLowerCase(char c);
 
 int main()
 {
-
-	//cout << (int) 'a';
-	//cout << (int) 'e';
-
 	//vars
 	string lastName;
 	char initial;
@@ -34,6 +30,7 @@ int main()
 	if (balance == 0)
 	{
 		cout << "Hall 18";
+		return 0;
 	}
 	else
 	{
@@ -41,16 +38,38 @@ int main()
 		if (initial >= 97 && initial <= 101)
 		{
 			cout << "Hall 3";
+			return 0;
+		}
+		else if (initial >= 102 && initial <= 106)
+		{
+			cout << "Hall 2";
+			return 0;
+		}
+		else if (initial >= 107 && initial <= 111)
+		{
+			cout << "Hall 8";
+			return 0;
+		}
+		else if (initial >= 112 && initial <= 114)
+		{
+			cout << "Hall 10";
+			return 0;
+		}
+		else if (initial >= 1145 && initial <= 122)
+		{
+			cout << "Hall 12";
+			return 0;
 		}
 	}
 
-	//output
-
-	return 0;
+	//If the program reaches this far, the user entered invalid input
+	return -1;
 }
 
 char toLowerCase(char c)
 {
+	//If the character is in the range of uppercase characters,
+	//adding 32 to its binary representation will make it lowercase
 	if (c < 65 || c > 90) return -1;
 	return c + 32;
 }
