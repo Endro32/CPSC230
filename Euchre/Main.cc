840478e81985588a4578c4751ba922773b7baa09
Endro32 <webk727@gmail.com>
2019-09-17 23:34:24 -0400

    Added space function and finished board design

--- Euchre/Main.cc
+++ Euchre/Main.cc
@@ -16,6 +16,7 @@ using namespace std;
 void render(string);
 string getCardLine(int, string, string);
 string getCardStackLine(int, int);
+string space(int);
 
 namespace suits
 {
@@ -28,9 +29,8 @@ namespace suits
 int main()
 {
     SetConsoleOutputCP(65001);
-    string rank;
-    cin >> rank;
-	render(rank);
+    
+	render("A");
 }
 
 /**
@@ -38,9 +38,18 @@ int main()
  */
 void render(string rank)
 {
+    // Board size: 80x32 (might make it 80x30 at some point)
+    
     system("cls");
     
-    cout << getCardLine(0, rank, suits::hearts) << endl;
+    cout << space(37) << "Jarvis\n"
+         << space(10) << "Hands won:" << space(12) << getCardStackLine(0, 5)
+         << space(9) << "Tricks won:\n" << space(10) << "You: 5" << space(16)
+         << getCardStackLine(1,5) << space(9) << "Stefan: 2\n"
+         << space(10) << "Opponent: 3" << space(11) << getCardStackLine(2, 5);
+    
+    
+   /* cout << getCardLine(0, rank, suits::hearts) << endl;
     cout << getCardLine(1, rank, suits::hearts) << endl;
     cout << getCardLine(2, rank, suits::hearts) << endl;
     cout << getCardLine(3, rank, suits::hearts) << endl;
@@ -54,7 +63,7 @@ void render(string rank)
     cout << getCardStackLine(3, 5) << endl;
     cout << getCardStackLine(4, 5) << endl;
     cout << getCardStackLine(5, 5) << endl;
-    cout << getCardStackLine(6, 5);
+    cout << getCardStackLine(6, 5);*/
 }
 
 /**
@@ -108,6 +117,11 @@ string getCardLine(int line, string rank, string suit)
     return ret;
 }
 
+/**
+ * Returns a line of a flipped over card stack
+ * int line: Line, from 0 to 6
+ * int numberOfCards: Number of cards in the stack
+ */
 string getCardStackLine(int line, int numberOfCards)
 {
     string ret = "";
@@ -119,5 +133,11 @@ string getCardStackLine(int line, int numberOfCards)
         concat = "─┘";
     for(int i = numberOfCards; --i; i > 0)
         ret += concat;
+    return ret;
+}
+
+string space(int spaces)
+{
+    string ret(spaces, ' ');
     return ret;
 }
